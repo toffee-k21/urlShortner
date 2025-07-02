2714319e0196b9881076a5f8365bb2a480e81a2a
Taufiq <taufiq9.2004@gmail.com>
2025-07-02 12:23:31 +0530

    url redirect fix

--- Index.js
+++ Index.js
@@ -20,18 +20,29 @@ app.use(express.urlencoded({ extended: false }));
 
 app.set('view engine', 'ejs');//bta do konsa template engine use kr rhe ho
 app.set('views', path.resolve('./views'));//kaha pr h
-
+app.get('/',(req, res)=>{
+  res.send("hello");
+})
 app.use('/url',restrictLoggedInUserOnly,useUrlRouter);
-app.use('/user',signRouter);
-app.get('/show/:shortId',async (req,res)=>{
+app.use('/auth',signRouter);
+app.get('/:shortId',async (req,res)=>{
     const mainUrl = await URL.findOneAndUpdate(
-        { shortID: req.params.shortId },
-        {
-          $push: { visitHistory: Date.now() },
-        }
-      );
-    
-      res.redirect(mainUrl.redirectUrl);
+      { shortID: req.params.shortId },
+      {
+        $push: { visitHistory: Date.now() },
+      }
+    );
+    if(!mainUrl || !mainUrl.redirectUrl){
+      res.send('no url registered for this token !');
+      return;
+    }
+    if (
+      mainUrl.redirectUrl.startsWith("http://") ||
+      mainUrl.redirectUrl.startsWith("https://")
+    ) {
+      res.redirect(mainUrl?.redirectUrl);
+    } else res.redirect("http://" + mainUrl.redirectUrl);
+    return;
 })
 
 app.listen(PORT,()=>console.log(`server started at port : ${PORT}`));
